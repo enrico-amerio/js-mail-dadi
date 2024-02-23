42042b4e54293d7fcdaa1915e71f7428c3fb41af
enrico-amerio <amerio.enrico95@gmail.com>
2024-02-23 18:14:29 +0100

    [add] Graphic to Dice

--- Dadi/JS/script.js
+++ Dadi/JS/script.js
@@ -1,38 +1,50 @@
 
-const userName = prompt('come ti chiami?')
-
-const userNumber = Math.floor(Math.random() * 6) + 1;
-const cpuNumber = Math.floor(Math.random() * 6) + 1;
-console.log(userNumber, cpuNumber)
+const userName = prompt('Come ti chiami?')
+const btnPlay = document.getElementById('press-to-play')
 
 document.querySelector('h1').innerHTML = `
     Benvenuto ${userName}!
     
     `
-document.getElementById('userDice').innerHTML = `
-Il numero scelto per ${userName} è ${cpuNumber}
 
 
-`
-document.getElementById('computerDice').innerHTML = `
-Il numero scelto per Computer è ${userNumber}
 
+btnPlay.addEventListener("click", function() {
+  const userNumber = Math.floor(Math.random() * 6) + 1;
+  const cpuNumber = Math.floor(Math.random() * 6) + 1;
+  console.log(userNumber, cpuNumber)
+  
+  document.getElementById('userDice').innerHTML = `
+  Il numero estratto per ${userName} è <b>${cpuNumber}</b>
+  
+  
+  `
+  document.getElementById('computerDice').innerHTML = `
+  Il numero estratto per Computer è <b>${userNumber}</b>
+  
+  
+  `
+  if (userNumber > cpuNumber) {
+    document.getElementById('result').innerHTML = `
+    Complimenti ${userName}, hai vinto contro il Computer!
+    <img class="emoji" src="https://static.wixstatic.com/media/e6f56d_a2b47380e8504300bfb2844e4a8a5159~mv2.gif" alt="">
 
-`
-if (userNumber > cpuNumber) {
-  document.getElementById('result').innerHTML = `
-    Complimenti ${userName}, hai vinto contro il computer!
     
     `
-}else if(cpuNumber > userNumber) {
-  document.getElementById('result').innerHTML = `
-    Ci dispiace ${userName}, questa volta ha vinto il computer!
-    
+  }else if(cpuNumber > userNumber) {
+    document.getElementById('result').innerHTML = `
+    Ci dispiace ${userName}, questa volta ha vinto il Computer!
+    <img class="emoji" src="https://media1.tenor.com/m/nRcjFCeXtPIAAAAC/sad-banana.gif" alt="">
+
     `
-} else {
-  document.getElementById('result').innerHTML = `
+  } else {
+    document.getElementById('result').innerHTML = `
     C\'eri quasi ${userName}!
     La partita termina con un pareggio
-    
+    <img class="emoji" src="https://media1.tenor.com/m/xsvqUaK8Z2AAAAAC/sport-exercise.gif" alt="">
+
     `
-}
+  }
+  btnPlay.innerText = "Gioca Ancora"
+  
+});
